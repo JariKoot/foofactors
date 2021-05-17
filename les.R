decde1f3ee971fdda6a999e7ebfc102109f5fae2
JariKoot <jarikoot@gmail.com>
2021-05-17 10:25:03 +0200

    Updates fbind()

---
+++ les.R
@@ -0,0 +1,17 @@
+library(devtools)
+library(tidyverse)
+library(fs)
+
+(a <- factor(c("character", "hits", "your", "eyeballs")))
+#> [1] character hits      your      eyeballs
+#> Levels: character eyeballs hits your
+(b <- factor(c("but", "integer", "where it", "counts")))
+#> [1] but      integer  where it counts
+#> Levels: but counts integer where it
+c(a, b)
+#> [1] 1 3 4 2 1 3 4 2
+factor(c(as.character(a), as.character(b)))
+
+fbind(a, b)
+
+exists("fbind", where = globalenv(), inherits = FALSE)
